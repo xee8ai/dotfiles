111183ddec38266e600369afedca4820e60b5933
xee8ai <xee8ai@users.noreply.github.com>
2015-09-29 16:46:29 +0200

    Added script to populate .vim
    - get pathogen
    - install plugins in use

---
+++ populate_dotvim.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+HOMEDIR="~"
+DIRS=".vim
+.vim/autoload
+.vim/bundle
+dotfiles"
+
+CURDIR=$(pwd)
+
+echo
+
+echo "Making dirs…"
+echo "------------"
+# make dirs
+for DIR in $DIRS; do
+	if ! test -e $HOMEDIR/$DIR; then
+		mkdir $HOMEDIR/$DIR
+	fi
+done
+echo
+
+cd $HOMEDIR/.vim/autoload
+
+# get pathogen
+echo "Cloning pathogen…"
+echo "-----------------"
+git clone https://github.com/tpope/vim-pathogen
+echo
+
+# get plugins
+cd $HOMEDIR/.vim/bundle
+echo "Cloning plugins…"
+echo "----------------"
+git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
+echo
+
+cd $CURDIR
