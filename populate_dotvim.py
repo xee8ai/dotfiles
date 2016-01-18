1c687055d7aaff10abe78e419efd4ed767b4ac97
xee8ai <xee8ai@users.noreply.github.com>
2016-01-18 09:26:10 +0100

    Finished python script for installing/updating
    Now we can auto-perform these tasks also on windows :-)

--- populate_dotvim.py
+++ populate_dotvim.py
@@ -15,8 +15,8 @@ class DotvimPopulator():
     plugin_sources = [
             'https://github.com/mattn/emmet-vim.git',
             'https://github.com/scrooloose/nerdtree.git',
-            'git clone git://github.com/altercation/vim-colors-solarized.git',
-            'git clone https://github.com/scrooloose/syntastic.git',
+            'git://github.com/altercation/vim-colors-solarized.git',
+            'https://github.com/scrooloose/syntastic.git',
             'https://github.com/jistr/vim-nerdtree-tabs.git',
             'https://github.com/tpope/vim-surround.git',
             'https://github.com/tpope/vim-commentary.git',
@@ -24,10 +24,44 @@ class DotvimPopulator():
     ]
 
 
+
 ################################################################################
     def __init__(self):
         '''Constructor.'''
+
+        self._define_tasks()
+        self._check_args()
         self._set_environment()
+
+        # run selected task
+        self.tasks[self.task]()
+
+
+################################################################################
+    def _define_tasks(self):
+        '''Define possible tasks and methods to call.'''
+
+        self.tasks = {
+                'install': self.install,
+                'update': self.update,
+        }
+
+
+################################################################################
+    def _check_args(self):
+        '''Check given CLI.'''
+
+        def error_func():
+            print('Usage: {} [{}]'.format(sys.argv[0], '|'.join(self.tasks)))
+            sys.exit(1)
+
+        if len(sys.argv) != 2:
+            error_func()
+
+        if sys.argv[1] not in self.tasks:
+            error_func()
+
+        self.task = sys.argv[1]
 
 
 ################################################################################
@@ -64,12 +98,14 @@ class DotvimPopulator():
             # set environment generic
             self.home_dir = os.getenv('HOME')
             self.vim_dir = os.path.join(self.home_dir, '.vim')
-            print(self.vim_dir)
+            print('vim dir is {}: '.format(self.vim_dir))
         elif self.platform is 'win':
             # set hardcoded (as there is only one Windows I have to use)
-            self.vim_dir = 'w:\vimfiles'
+            self.vim_dir = 'w:\\vimfiles'
 
         self.bundle_dir = os.path.join(self.vim_dir, 'bundle')
+
+        print()
 
 
 ################################################################################
@@ -86,22 +122,31 @@ class DotvimPopulator():
 
 ################################################################################
     def _install_pathogen(self):
-        '''Installes pathogen (if not yet installed).'''
+        '''Installes pathogen (use also for updating).'''
+
+        print('(Re)Installing pathogen…')
 
         with urllib.request.urlopen('https://tpo.pe/pathogen.vim') as response, open(os.path.join(self.vim_dir, 'autoload', 'pathogen.vim'), 'wb') as out_file:
             data = response.read() # a byte object
             out_file.write(data)
+
+        print()
 
 
 ################################################################################
     def _install_plugin(self, name, source):
         '''Installes a given plugin (if not yet installed).'''
 
+        print('Installing {}…'.format(name))
+
         if os.path.exists(os.path.join(self.bundle_dir, name)):
-            print('{} is already installed'.format(name))
+            print('Already installed.'.format(name))
+            print()
             return True
 
         subprocess.call(['git', 'clone', source, os.path.join(self.bundle_dir, name)])
+
+        print()
 
 
 ################################################################################
@@ -120,12 +165,35 @@ class DotvimPopulator():
 
 
 ################################################################################
+    def _update_plugin(self, name):
+        '''Updates a plugin with git pull.'''
+
+        print('Updating {}: '.format(name))
+
+        plugin_dir = os.path.join(self.bundle_dir, name)
+
+        if not os.path.isdir(plugin_dir):
+            print('{} not yet installed. Skipping…'.format(name))
+
+        os.chdir(plugin_dir)
+        subprocess.call(['git', 'pull'])
+
+        print()
+
+
+################################################################################
     def update(self):
         '''Update plugins.'''
 
         # just re-install
         self._install_pathogen()
-        pass
+
+        # update plugins using git
+        for source in self.plugin_sources:
+            name = self._get_plugin_name_from_source(source)
+            if name is None:
+                continue
+            self._update_plugin(name)
 
 
 
@@ -133,4 +201,3 @@ class DotvimPopulator():
 ################################################################################
 if __name__ == '__main__':
     populator = DotvimPopulator()
-    populator.install()
