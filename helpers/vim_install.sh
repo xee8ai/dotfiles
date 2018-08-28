272c5a0d99e22e37fd683f3be733e958e14bb8fb
xee8ai <xee8ai@users.noreply.github.com>
2018-08-28 14:22:04 +0200

    Destination dir now generic for vim install.

--- helpers/vim_install.sh
+++ helpers/vim_install.sh
@@ -3,8 +3,15 @@
 SRC_DIR="$HOME/src"
 VIM_SRC_DIR="$SRC_DIR/vim"
 
+DST_DIR="$HOME/.local"
+VIM_DST_DIR="$DST_DIR/vim"
+
 if [ ! -d $SRC_DIR ]; then
 	mkdir -p $SRC_DIR
+fi
+
+if [ ! -d $DST_DIR ]; then
+	mkdir -p $DST_DIR
 fi
 
 cd $SRC_DIR
@@ -20,6 +27,6 @@ fi
 cd $VIM_SRC_DIR/src
 
 # see ./configure --help for available options
-./configure --enable-cscope --enable-gui=no --enable-luainterp=yes --enable-multibyte --enable-python3interp --enable-rubyinterp=yes --enable-tclinterp=yes --prefix=/root/.local/vim --with-features=huge --with-tlib=ncurses --without-x
+./configure --enable-cscope --enable-gui=no --enable-luainterp=yes --enable-multibyte --enable-python3interp --enable-rubyinterp=yes --enable-tclinterp=yes --prefix=$VIM_DST_DIR --with-features=huge --with-tlib=ncurses --without-x
 
 make && make install
