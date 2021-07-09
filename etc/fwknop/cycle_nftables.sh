1983ef9050a2a571a8e394a973ad65a7737d6922
xee8ai <xee8ai@users.noreply.github.com>
2021-07-09 12:46:53 +0200

    fwknopd: Add conf, helper and apparmor profile to use nftables.

---
+++ etc/fwknop/cycle_nftables.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NFT="/sbin/nft"
+CUT="/usr/bin/cut"
+GREP="/usr/bin/grep"
+HEAD="/usr/bin/head"
+SED="/usr/bin/sed"
+
+TABLE="inet filter"
+CHAIN="FWKNOP_INPUT"
+COMMENT="FWKNOP_CREATED_RULE"
+
+JOB=$1
+IP=$2
+PORTS=$3
+
+if [ "$JOB" == "open" ]; then
+	1/0
+	CMD="$NFT add rule $TABLE $CHAIN ip saddr $IP tcp dport { $PORTS } accept comment \"$COMMENT\""
+	$CMD
+elif [ "$JOB" == "close" ]; then
+	HANDLE=$(nft -a list ruleset | $GREP $IP | $GREP $COMMENT | $CUT -d '#' -f2 | $SED 's/\s//g' | $SED 's/handle//g' | $HEAD -n 1)
+	CMD="$NFT delete rule $TABLE $CHAIN handle $HANDLE"
+	$CMD
+else
+	exit 1
+fi
