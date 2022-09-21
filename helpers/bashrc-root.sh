670f16aa89f736155f1cea7c4265172c4c6dddcd
xee8ai <xee8ai@users.noreply.github.com>
2022-09-21 11:15:53 +0200

    bashrc helper: Overwrite /root/.gitconfig.

--- helpers/bashrc-root.sh
+++ helpers/bashrc-root.sh
@@ -13,16 +13,16 @@ USERNAME=$(echo "$SCRIPT_DIR" | cut -d'/' -f3)
 
 FILEPATH="/home/$USERNAME/.mybash/liquidprompt/liquidprompt"
 if [ -e $FILEPATH ]; then
-	source $FILEPATH
+    source $FILEPATH
 else
-	echo "Ignoring non-existing $FILEPATH…"
+    echo "Ignoring non-existing $FILEPATH…"
 fi
 
 FILEPATH="/home/$USERNAME/dotfiles/.bashrc__general__aliases_exports"
 if [ -e $FILEPATH ]; then
-	source $FILEPATH
+    source $FILEPATH
 else
-	echo "Ignoring non-existing $FILEPATH…"
+    echo "Ignoring non-existing $FILEPATH…"
 fi
 
 LOCALVIMPATH="/home/$USERNAME/.local/vim/bin/vim"
@@ -35,3 +35,14 @@ fi
 alias vimr="$VIM -u /home/$USERNAME/dotfiles/.vimrc --cmd 'set rtp^=/home/$USERNAME/.vim' -R -p"
 alias vim="$VIM -u /home/$USERNAME/dotfiles/.vimrc --cmd 'set rtp^=/home/$USERNAME/.vim' -p"
 alias vimdiff="$VIM -u /home/$USERNAME/dotfiles/.vimrc --cmd 'set rtp^=/home/$USERNAME/.vim' -d"
+
+# check if we want to symlink to users .gitconfig, too
+# this is done if “/root/.gitconfig-root” exists
+GITCONFIG_ROOT="/root/.gitconfig"
+GITCONFIG_ROOT_BACKUP="/root/.gitconfig-root"
+GITCONFIG_USER="/home/$USERNAME/.gitconfig"
+if [ -e $GITCONFIG_ROOT_BACKUP ]; then
+    echo "Symlinking $GITCONFIG_ROOT to $GITCONFIG_USER"
+    rm -f $GITCONFIG_ROOT
+    ln -s $GITCONFIG_USER $GITCONFIG_ROOT
+fi
