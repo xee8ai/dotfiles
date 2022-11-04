a5f6e23a567c9f474187c629008635fcffedfaf1
xee8ai <xee8ai@users.noreply.github.com>
2022-11-04 18:14:28 +0100

    Add output to nft open script.

--- root/bin/nft-open-for-hostname.sh
+++ root/bin/nft-open-for-hostname.sh
@@ -6,6 +6,8 @@ if [ "$#" -lt 1 ]; then
 	echo "Usage: $0 [open|close]"
 fi
 
+echo
+
 NFT="/sbin/nft"
 CUT="/usr/bin/cut"
 GREP="/usr/bin/grep"
@@ -24,15 +26,25 @@ PORTS=22
 
 
 IPS=$(dig +short $HOSTNAME)
+echo "Got the following IPs for $HOSTNAME:"
+for IP in $IPS; do
+    echo "  $IP"
+done
+echo
+
 for IP in $IPS; do
     if [ "$JOB" == "open" ]; then
         CMD="$NFT add rule $TABLE $CHAIN ip saddr $IP tcp dport { $PORTS } accept comment \"$COMMENT\""
+        echo "$CMD"
         $CMD
     elif [ "$JOB" == "close" ]; then
         HANDLE=$(nft -a list ruleset | $GREP $IP | $GREP $COMMENT | $CUT -d '#' -f2 | $SED 's/\s//g' | $SED 's/handle//g' | $HEAD -n 1)
         CMD="$NFT delete rule $TABLE $CHAIN handle $HANDLE"
+        echo "$CMD"
         $CMD
     else
         exit 1
     fi
 done
+
+echo
