ceb512be3e033785a6c6d7e0b6a3c765f3eb8d31
xee8ai <xee8ai@users.noreply.github.com>
2025-04-29 08:17:20 +0200

    tmp

--- populate_dotvim.py
+++ populate_dotvim.py
@@ -243,6 +243,7 @@ class DotvimPopulator():
 
         if not os.path.isdir(plugin_dir):
             print('{} not yet installed. Skipping...'.format(name))
+            print()
             return
 
         os.chdir(plugin_dir)
